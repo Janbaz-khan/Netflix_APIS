c04794c47adca7aba69749746209f6ec70f5a079
Janbaz khan <janbazofficial20@gmail.com>
2021-12-16 17:21:56 +0500

    unit testing

---
+++ UnitTest/GenreController_Test.cs
@@ -0,0 +1,46 @@
+﻿using HomeTag_Assignment.Controllers;
+using HomeTag_Assignment.Model;
+using HomeTag_Assignment.Repository.GenreRepository;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace HomiTag_UnitTests
+{
+   public class GenreController_Test
+    {
+        private readonly GenreController _controller;
+        private readonly IGenreRepository _service;
+
+        public GenreController_Test()
+        {
+            //_service = new IGenreRepository();
+            _controller = new GenreController(_service);
+        }
+
+        [Fact]
+        public void Get_WhenCalled_ReturnsOkResult()
+        {
+            // Act
+            var okResult = _controller.GetGenres();
+
+            // Assert
+            Assert.IsType<OkObjectResult>(okResult);
+        }
+
+        [Fact]
+        public void Get_WhenCalled_ReturnsAllItems()
+        {
+            // Act
+            var okResult = _controller.GetGenres();
+
+            // Assert
+            var items = Assert.IsType<List<Genre>>(okResult);
+            Assert.Equal(2, items.Count);
+        }
+    }
+}
---
+++ UnitTest/MovieController_Test.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HomeTag_Assignment.Controllers;
+using HomeTag_Assignment.Model;
+using HomeTag_Assignment.Repository.MovieRepository;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+
+namespace HomiTag_UnitTests
+{
+  public class MovieController_Test
+    {
+            private readonly MovieController _controller;
+            private readonly IMovieRepository _service;
+
+            public MovieController_Test()
+            {
+                //_service = new IMovieRepository();
+                _controller = new MovieController(_service);
+            }
+
+            [Fact]
+            public void Get_WhenCalled_ReturnsOkResult()
+            {
+                // Act
+                var okResult = _controller.GetMovies();
+
+                // Assert
+                Assert.IsType<OkObjectResult>(okResult);
+            }
+
+            [Fact]
+            public void Get_WhenCalled_ReturnsAllItems()
+            {
+                // Act
+                var okResult = _controller.GetMovies();
+
+                // Assert
+                var items = Assert.IsType<List<Movie>>(okResult);
+                Assert.Equal(2, items.Count);
+            }
+        }
+    
+}
